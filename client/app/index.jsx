15bc7da5b05706e49c85bf280241888fa7dd94ef
Michael Dwyer <mcdwyer1@gmail.com>
2017-03-22 16:26:33 -0400

    Add route

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
-import { Provider } from 'react-redux';
 
 import App from './app.jsx';
 import About from './about.jsx';
+import Department from './department.jsx';
 
 import { Provider } from 'react-redux';
 import store, { history } from '../store';
@@ -14,8 +14,10 @@ const router = (
     <Router history={history}>
       <Route path='/' component={App}>
         <IndexRoute component={About} />
+        <Route path='/:departmentName' component={Department} />
       </Route>
     </Router>
   </Provider>
 );
+
 render(router, document.getElementById('app'));
